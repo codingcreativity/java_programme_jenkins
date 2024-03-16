163d27b29039b19ca3eed10fe25d6ede7eeb0e1d
Jay Gohil <gohiljay0882430@gmail.com>
2024-03-16 23:38:18 +0530

    added student class

---
+++ Student.java
@@ -0,0 +1,9 @@
+public class Student {
+    public Student(){
+        System.out.println("Hello I'm in student class.");
+    }
+
+    public void sayHello(){
+        System.out.println("Hi, I am Jay Gohil.");
+    }
+}
